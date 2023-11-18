a9a6f0e4fc8c55da7d166bb46e7cbec1ad317d3d
unknown <yipy214@gmail.com>
2023-11-19 02:09:09 +0800

    Changed Metadata.cs to Markers.cs; modified I prefix to IM prefix for all marker interfaces.

---
+++ Markers.cs
@@ -0,0 +1,7 @@
+namespace Markers
+{
+    public interface IMCanBeParent<T>{}
+    public interface IMDependee{}
+    public interface IMExpaRelatedObject { }
+    
+}
--- Metadata.cs
+++
@@ -1,7 +0,0 @@
-namespace Metadata
-{
-    public interface ICanBeParent<T>{}
-    public interface IDependee{}
-    public interface IExpaRelatedObject { }
-    
-}
