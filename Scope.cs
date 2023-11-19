373988da3160f358e14739540df1f6e5db40f050
unknown <yipy214@gmail.com>
2023-11-20 07:24:42 +0800

    Changed TokenIdentifier to string Identifier to comply with design changes

--- Scope.cs
+++ Scope.cs
@@ -5,13 +5,13 @@ namespace Structs
 {
     internal readonly struct Scope
     {
-        public readonly Token TokenIdentifier { get; init; }
+        public readonly string Identifier { get; init; }
         public readonly TokenType TType { get; init; }
         public readonly Token[] Code { get; init; }
         public ArgumentList? Arguments{ get; }
-        public Scope(Token aIdentifier, TokenType aType, Token[] aCode)
+        public Scope(string Identifier, TokenType aType, Token[] aCode)
         {
-            TokenIdentifier = aIdentifier;
+            this.Identifier = Identifier;
             Code = aCode;
             TType = aType;
         }
